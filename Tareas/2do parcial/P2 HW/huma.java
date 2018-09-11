8002ec1ea58da1a534171fb0636ef3b47434d6f9
Juan Pablo Velazco <juanpa.jpvv@gmail.com>
2018-09-11 13:34:16 -0500

    H2.2

---
+++ Tareas/2do parcial/P2 HW/huma.java
@@ -0,0 +1,33 @@
+public class huma{
+
+  protected double speed;
+  protected double fuerza;
+  protected String name;
+
+
+	public double getSpeed() {	return speed;}
+	public double getFuerza() {	return fuerza;}
+	public String getName() {	return name;}
+
+
+  public huma( double speed, double fuerza, String name){
+    this.speed = speed;
+    this.fuerza = fuerza;
+    this.name = name;
+  }
+
+  public static void run(){
+    System.out.println("GOTTA GO FAST");
+  }
+
+  public static void run(String name){
+    System.out.println("GOTTA GO FASTER "+name);
+  }
+
+  public static void run(double fuerza){
+    System.out.println("GOTTA GO FAST WITH "+fuerza+" NEWTONS ");
+  }
+
+
+
+}
