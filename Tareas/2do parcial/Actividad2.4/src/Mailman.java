67dabc6b379402fd92d7c7b4d49c95454ca89aa9
JuanPyV <juanpa.jpvv@gmail.com>
2018-09-24 21:01:14 -0500

    Tarea 2.4

---
+++ Tareas/2do parcial/Actividad2.4/src/Mailman.java
@@ -0,0 +1,16 @@
+
+public class Mailman {
+
+		String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public Mailman(String name) {
+			super();
+			this.name = name;
+		}
+		
+		
+}
